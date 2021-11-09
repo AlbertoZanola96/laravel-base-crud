d0a09b2df34c70fcfa39a8efd07b1bcf60f2a766
AlbertoZanola <albertozanola1996@gmail.com>
2021-11-09 13:47:05 +0100

    Completamento con correzione

--- resources/views/fumettis/edit.blade.php
+++ resources/views/fumettis/edit.blade.php
@@ -9,31 +9,31 @@
                     @method('PUT')
                     <div class="mb-3">
                       <label for="title">Title</label>
-                      <input value="{{ $fumetti['title'] }}" type="text" name="title" id="title" class="form-control" placeholder="Enter name of Comic">
+                      <input value="{{ $fumetto['title'] }}" type="text" name="title" id="title" class="form-control" placeholder="Enter name of Comic">
                     </div>
                     <div class="mb-3">
                       <label for="description" class="form-label">Description</label>
-                      <input value="{{ $fumetti['description'] }}" type="text" name="description" class="form-control" id="description" placeholder="Enter comic description">
+                      <input value="{{ $fumetto['description'] }}" type="text" name="description" class="form-control" id="description" placeholder="Enter comic description">
                     </div>
                     <div class="mb-3">
                         <label for="image" class="form-label">Image</label>
-                        <input value="{{ $fumetti['image'] }}" type="text" name="image" class="form-control" id="image" placeholder="Enter comic image">
+                        <input value="{{ $fumetto['image'] }}" type="text" name="image" class="form-control" id="image" placeholder="Enter comic image">
                     </div>
                     <div class="mb-3">
                         <label for="price" class="form-label">Price</label>
-                        <input value="{{ $fumetti['price'] }}" type="text" name="price" class="form-control" id="price" placeholder="Enter comic price">
+                        <input value="{{ $fumetto['price'] }}" type="text" name="price" class="form-control" id="price" placeholder="Enter comic price">
                     </div>
                     <div class="mb-3">
                         <label for="series" class="form-label">Series</label>
-                        <input value="{{ $fumetti['series'] }}" type="text" name="series" class="form-control" id="series" placeholder="Enter comic series">
+                        <input value="{{ $fumetto['series'] }}" type="text" name="series" class="form-control" id="series" placeholder="Enter comic series">
                     </div>
                     <div class="mb-3">
                         <label for="sale_date" class="form-label">Sale Date</label>
-                        <input value="{{ $fumetti['sale_date'] }}" type="text" name="sale_date" class="form-control" id="sale_date" placeholder="Enter comic sale date">
+                        <input value="{{ $fumetto['sale_date'] }}" type="text" name="sale_date" class="form-control" id="sale_date" placeholder="Enter comic sale date">
                     </div>
                     <div class="mb-3">
                         <label for="type" class="form-label">Type</label>
-                        <input value="{{ $fumetti['type'] }}" type="text" name="type" class="form-control" id="type" placeholder="Enter comic type">
+                        <input value="{{ $fumetto['type'] }}" type="text" name="type" class="form-control" id="type" placeholder="Enter comic type">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
